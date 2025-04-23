a8fd3db744fc43e54187f5851834d3e0e80c46ea
Halluc1nations <Blaine.Bishop@comcast.net>
2025-04-22 22:23:13 -0400

    connection.ts update

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,18 +3,18 @@ dotenv.config();
 
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "";
-
-const db = async (): Promise<mongoose.Connection> => {
-  try {
-    await mongoose.connect(
-      MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
-    );
-    console.log("DB connected");
-    return mongoose.connection;
-  } catch (error) {
-    console.error("DB connection error:", error);
-    throw new Error("DB connection failed");
-  }
-};
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks";
+mongoose.connect(connectionString);
+const db = mongoose.connection;
+//   try {
+//     await mongoose.connect(
+//       connectionString,
+//     );
+//     console.log("DB connected");
+//     return mongoose.connection;
+//   } catch (error) {
+//     console.error("DB connection error:", error);
+//     throw new Error("DB connection failed");
+//   }
 export default db;
